a7c6974c5a51f4a017f888baa9c35f49b09330d6
Josué Serejo <josueserejo5@gmail.com>
2026-04-26 10:50:44 -0300

    test 1: criar conta bancária

--- pbackend/testes_unitarios/banco/tests/bancoService.spec.ts
+++ pbackend/testes_unitarios/banco/tests/bancoService.spec.ts
@@ -1,3 +1,3 @@
-import {describe, it, expect} from 'vitest';
+import {describe, it, expect, beforeEach} from 'vitest';
 import { BancoService } from '../src/bancoService';
 
--- pbackend/testes_unitarios/banco/tests/contaBancaria.spec.ts
+++ pbackend/testes_unitarios/banco/tests/contaBancaria.spec.ts
@@ -1,4 +1,20 @@
-import {describe, it, expect} from 'vitest';
+import {describe, it, expect, beforeEach} from 'vitest';
 import { ContaBancaria } from '../src/contaBancaria';
+
+describe('ContaBancaria', () => {
+    let conta: ContaBancaria;
+
+    beforeEach(() => {
+        conta = new ContaBancaria('12345', 'João Silva', 1000);
+    });
+
+    it('Deve criar a conta com os dados corretos', () => { 
+        expect(conta.numero).toBe('12345');
+        expect(conta.titular).toBe('João Silva');
+        expect(conta.consultarSaldo()).toBe(1000);
+
+    });
+
+});
 
 
